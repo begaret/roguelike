9ff597b1a709f31317e221827fc677f313e87e40
Begaret <olofellestad@gmail.com>
2023-12-20 15:01:29 +0100

    added help screen

--- src/help_menu.c
+++ src/help_menu.c
@@ -3,19 +3,33 @@
 #include "term.h"
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
+// TODO: add scrolling and make prettier
 void help_menu(void)
 {
+	FILE *fp = fopen("help.txt", "r");
+	if (fp == NULL) {
+		return;
+	}
+
+	tclear();
+	
+	tcolor(C_WHITE, C_BLACK);
+	tborder(0, 0, -1, -1, "help", NULL);
+
+	int y = 2;
+	char line[128];
+	while (fgets(line, 128, fp) != NULL) {
+		tprintf(2, y++, "%.*s", strlen(line) - 1, line);
+	}
+
+	tflush();
+
+	fclose(fp);
 	while (1) {
-		tclear();
-
-		tcolor(C_WHITE, C_BLACK);
-		tborder(0, 0, -1, -1, "help", NULL);
-
-		tprintf(2, 2, "&YTODO");
-
-		tflush();
-		
 		int k = tgetc();
 		if (k == K_ESCAPE) {
 			return;
